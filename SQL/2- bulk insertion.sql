f25dc5daaff341a7573ca326e71edaa51bd0c19e
Ahmed Ayman <102385517+ahmedaymanb4@users.noreply.github.com>
2024-09-25 21:11:48 +0300

    Add files via upload

---
+++ SQL/2- bulk insertion.sql
@@ -0,0 +1,16 @@
+USE depi_final_project;
+-- Bulk add of the data in "/data source.xlsx" to this database:
+
+-- Vendors table
+
+-- Materials table
+
+-- Defects table
+
+-- Plants table
+
+-- Categories table
+
+-- Defect_Types table
+
+-- Defect_Items table
